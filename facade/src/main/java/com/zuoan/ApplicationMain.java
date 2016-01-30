0a76a0ac63cba6375a3b25023a70497c64deea15
haotengteng <haot_mail@163.com>
2016-01-30 15:46:23 +0800

    update

--- facade/src/main/java/com/zuoan/ApplicationMain.java
+++ facade/src/main/java/com/zuoan/ApplicationMain.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by XUJY on 2016/1/18.
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration//配置控制
 @EnableAutoConfiguration()//启用自动配置
 @ComponentScan//组件扫描
+@ImportResource(locations = "classpath*:spring-servlet.xml")
 public class ApplicationMain {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class);
